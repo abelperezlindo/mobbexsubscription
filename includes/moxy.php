2161186c5e1ef1a6cbe7296d8b3de34121f8fa54
abelpzl <abel.pzl.com@gmail.com>
2020-06-19 01:42:41 -0300

    Circuito completo minimamente probado

--- includes/moxy.php
+++ includes/moxy.php
@@ -3,25 +3,10 @@
 class Moxy
 {
 
-    private $postdata;
     private $apikey;
     private $accesstoken;
-    private $typesWithPayment = [
-        "subscription:registration",
-        "subscription:source_change",
-        "subscription:execution",
-    ];
-    public function __construct()
-    {
-        $params = func_get_args();
-        //saco el número de parámetros que estoy recibiendo
-        $num_params = func_num_args();
-        if ($num_params == 2 && method_exists($this, "__moxy")) {
-            //si existía esa función, la invoco, reenviando los parámetros que recibí en el constructor original
-            call_user_func_array(array($this, "__moxy"), $params);
-        }
-    }
-    public function __moxy($apikey, $accesstoken)
+
+    public function __construct($apikey, $accesstoken)
     {
         if (!empty($apikey)) {
             $this->apikey = $apikey;
@@ -32,76 +17,7 @@ class Moxy
         }
 
     }
-    public function setPost($post = null)
-    {
-        if ($post) {
-            $this->postdata = $post;
-        } else {
-            $this->postdata = file_get_contents("php://input");
-        }
 
-    }
-    public function setApiKey($apikey)
-    {
-        if (!empty($apikey)) {
-            $this->apikey = $apikey;
-        }
-
-    }
-    public function setAccessToken($accessToken)
-    {
-        if (!empty($accessToken)) {
-            $this->accessToken = $accessToken;
-        }
-
-    }
-    public function getPostString()
-    {
-        if (empty($this->postdata)) {
-            return 0;
-        }
-
-        return $this->postdata;
-    }
-    public function getPostArray()
-    {
-        if (empty($this->postdata)) {
-            return 0;
-        }
-
-        $obj = [];
-        parse_str($this->postdata, $obj);
-        return $obj;
-    }
-    public function proccessPost()
-    {
-        $obj = $this->getPostArray();
-
-        if (in_array($obj['type'], $this->typesWithPayment)) {
-
-            if (!isset($obj['data']) || $obj['data']['status'] != 200) {
-                return false;
-            }
-
-            if ($obj['data']['payment']['operation']['type'] == 'payment') {
-                $this->getPaymentData($obj['data']['payment']['id']);
-            }
-            return;
-        }
-        if ($obj['type'] == "") {
-            return;
-        }
-        if ($obj['type'] == "") {
-            return;
-        }
-    }
-    public function getPaymentData($idPayment)
-    {
-        if (!function_exists('curl_version') || empty($idPayment)) {
-            return 0;
-        }
-
-    }
     public function getSubscriptions($idSuscription = '')
     {
 
@@ -114,9 +30,8 @@ class Moxy
             $urlBase = $urlBase . '/' . $idSuscription;
         }
 
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
+        $curl    = curl_init();
+        $options = [
             CURLOPT_URL => $urlBase,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
@@ -129,9 +44,10 @@ class Moxy
                 "x-api-key: " . $this->apikey,
                 "x-access-token: " . $this->accesstoken,
             ],
-        ));
+        ];
+        curl_setopt_array($curl, $options);
 
-        $response = curl_exec($curl);
+        $response       = curl_exec($curl);
         $responseDecode = json_decode($response, true);
         curl_close($curl);
 
@@ -193,7 +109,7 @@ class Moxy
         }
         $urlBase = 'https://api.mobbex.com/p/subscriptions/';
         $urlBase .= $idSuscription;
-        $urlBase -= '/subscriber';
+        $urlBase .= '/subscriber';
         $curl    = curl_init();
         $options = [
             CURLOPT_URL => $urlBase,
@@ -273,5 +189,46 @@ class Moxy
         return $responseDecode;
 
     }
+
+}
+
+
+class MoxyWebhook
+{
+    private $postdata;
+    public function __construct()
+    {
+        $this->postdata = file_get_contents("php://input");
+    
+    }
+
+    public function getPostString()
+    {
+        if (empty($this->postdata)) {
+            return 0;
+        }
+
+        return $this->postdata;
+    }
+    public function getPostArray($option = null)
+    {
+        if (empty($this->postdata)) {
+            return 0;
+        }
+        $obj = [];
+        parse_str($this->postdata, $obj);
+        
+        if(is_null($option)){
+            return $obj;
+        }
+        elseif($option == 'data'){
+            return $obj['data'];
+        }
+        elseif($option == 'type'){
+            return $obj['type'];
+        }
+    }
+
+
 
 }
